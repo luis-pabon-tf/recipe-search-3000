a262e8fb46eca05cf249b1f23df10eadf1446a1d
Luis Pabon <turkeyfried@gmail.com>
2025-05-22 11:15:01 -0400

    Add relationships

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Recipe extends Model
 {
     use HasFactory;
+
+    public function steps():HasMany
+    {
+        return $this->hasMany(Step::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function ingredients(): BelongsToMany
+    {
+        return $this->belongsToMany(Ingredient::class);
+    }
 }
