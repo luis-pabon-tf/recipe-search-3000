c3fefef134b4440fd29bb758d5dd8bfbe2f40f91
Luis Pabon <turkeyfried@gmail.com>
2025-05-22 18:20:16 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/search', SearchController::class);
+
+Route::get('/user/{slug}', [RecipeController::class, 'get']);
