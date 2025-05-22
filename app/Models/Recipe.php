31ef37644b5e0f4f2e0b8f4e285b910b9111a47a
Luis Pabon <turkeyfried@gmail.com>
2025-05-22 14:05:22 -0400

    fix many to many and slug

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -7,10 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Recipe extends Model
 {
     use HasFactory;
+
+     protected static function booted(): void
+     {
+        // generate slug during creation
+         static::creating(function (Recipe $recipe) {
+            // in real use slug should be made unique
+            $recipe->slug = Str::slug($recipe->name);
+         });
+
+     }
 
     public function steps():HasMany
     {
@@ -24,6 +35,6 @@ class Recipe extends Model
 
     public function ingredients(): BelongsToMany
     {
-        return $this->belongsToMany(Ingredient::class);
+        return $this->belongsToMany(Ingredient::class, 'recipe_ingredient');
     }
 }
