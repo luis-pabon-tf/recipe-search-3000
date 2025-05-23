b69f25cf10b3470bebd479913a9abce7552791d9
Luis Pabon <turkeyfried@gmail.com>
2025-05-22 20:40:16 -0400

    Updating the frontend to get the desired output

---
+++ frontend/components/SimpleTable.vue
@@ -0,0 +1,24 @@
+<!-- original vue version by @lizzieturney -->
+<script setup lang="ts">
+defineProps({
+    headers: Array,
+    data: Object
+})
+</script>
+
+<template>
+    <div>
+        <table>
+            <tr>
+                <th v-for="(header, i) in headers" :key="`${header}${i}`" class="header-item">
+                    {{ header }}
+                </th>
+            </tr>
+            <tr v-for="entry in data" :key="`entity-${entry.id}`" class="table-rows">
+                <td v-for="(header, i) in headers" :key="`${header}-${i}`">
+                    <slot :name="`column${i}`" :entry="entry"></slot>
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
-const email = defineModel()
-const keyword = defineModel()
-const ingredient = defineModel()
+const email = ""
+const keyword = ""
+const ingredient = ""
 
+const headers = [
+    'Name',
+    'Description',
+    'Author',
+    'Details'
+]
+
+let res: any = null
 async function handleFormSubmit() {
-  const res = await useFetch('localhost:8888/api/search', {
+  res = await useFetch('http://127.0.0.1:8888/api/search', {
     method: 'POST',
     body: {
         'email': email,
@@ -16,18 +24,46 @@ async function handleFormSubmit() {
 </script>
 
 <template>
-    <form @submit="handleFormSubmit">
-        <label for="email">Author email:</label><br>
-        <input v-model="email" placeholder="exact author email"><br>
+    <div>
+        <form @submit.prevent="handleFormSubmit">
+            <label for="email">Author email:</label><br>
+            <input v-model="email" placeholder="exact author email"><br>
 
-        <label for="keyword">Keyword:</label><br>
-        <input v-model="keyword" placeholder="name, description, ingredient or step"><br>
+            <label for="keyword">Keyword:</label><br>
+            <input v-model="keyword" placeholder="name, description, ingredient or step"><br>
 
-        <label for="ingredient">Ingredient:</label><br>
-        <input v-model="ingredient" placeholder="partial ingredient match"><br>
+            <label for="ingredient">Ingredient:</label><br>
+            <input v-model="ingredient" placeholder="partial ingredient match"><br>
 
-        <button type="submit">Search</button>
-    </form>
+            <button type="submit">Search</button>
+        </form>
+    </div>
+
+    <div>
+        <div v-if="res == null">
+            Do a search
+        </div>
+        <div v-else>
+            <SimpleTable :headers="headers" :data="res.data">
+                <template #column0="{ entry }">
+                {{ entry.name }}
+                </template>
+                <template #column1="{ entry }">
+                {{ entry.description }}
+                </template>
+                <template #column2="{ entry }">
+                {{ entry.email }}
+                </template>
+                <template #column3="{ entry }">
+                    <NuxtLink :to="'/recipe/' + entry.slug">View more</NuxtLink>
+                </template>
+            </SimpleTable>
+        </div>
+    </div>
     <NuxtLoadingIndicator />
-    <NuxtPage />
 </template>
+
+'Name',
+'Description',
+'Author',
+'Details'
