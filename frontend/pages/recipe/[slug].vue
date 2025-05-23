26d9f9b377d9604af36486ca9a0ffeca40bd98ea
Luis Pabon <turkeyfried@gmail.com>
2025-05-22 22:05:21 -0400

    Fix the recipe page route

---
+++ frontend/pages/recipe/[slug].vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const route = useRoute()
+const data  = await useFetch('/api/recipes/' + route.params.id)
+</script>
+
+<template>
+<div>
+    <div>
+  Recipe name
+
+  name
+
+  description
+
+  ingredients
+
+  steps
+
+  email
+
+  slug
+    </div>
+</div>
+</template>
