6caf673da2b8b01909e3e803308972284c1f3dc9
Luis Pabon <turkeyfried@gmail.com>
2025-05-22 23:01:54 -0400

    Adding test cases

--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -4,17 +4,46 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Author;
+use App\Models\Recipe;
+use App\Models\Ingredient;
 use Tests\TestCase;
 
 class SearchTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
+    public function testHappyPath(): void
+    {
+        $author = Author::factory()->create([
+            'email' => 'lolo_fuzzletoes@fakemail.com'
+        ]);
+
+        Recipe::factory()
+            ->count(50)
+            ->hasSteps(3)
+            ->for($author)
+            ->hasAttached(
+                Ingredient::factory()->count(3),
+                ['quantity' => 2]
+            )
+            ->create();
+
+        $response = $this->post('/api/search', [
+            'email' => $author->email,
+        ]);
         $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => [
+                'recipe' => [
+                    '*' => [
+                        'name',
+                        'age',
+                        'location'
+                    ]
+                ]
+            ]
+        ]);
     }
 }
